3d3d53a0ec27e21668b6ae7b412993be4866ceff
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-03-09 15:59:20 +0530

    Setup shadcn and fix some issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
-
+import { Button } from "@/components/ui/button";
 function App() {
   return (
     <>
       <h1 class="">Hello world!</h1>
+      <Button>Click Here</Button>
+      <Button>Click Here</Button>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+// import { ViteAliases } from "vite-aliases";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
