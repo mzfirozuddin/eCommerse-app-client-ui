35f9a25d13bea7216f68ed63ccd8531b112932bc
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-03-18 23:07:15 +0530

    add home page

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthLayout from "./Layout/AuthLayout";
 import DashboardLayout from "./Layout/DashboardLayout";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
+import HomePage from "@/pages/HomePage";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/auth",
     element: <AuthLayout />,
